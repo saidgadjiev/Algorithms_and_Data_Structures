d48a0b6971e37dd5327b09c954d76f8362ebd67a
Said Gadjiev <gsarma.1995@mail.ru>
2016-06-10 19:44:49 +0300

    Fix dequeue

---
+++ Deque/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include "deque.h"
+
+using namespace std;
+
+
+int main() {
+    Deque<std::string> deque;
+
+    for (int i = 0; i < 100; ++i) {
+        deque.pushBack("test" + std::to_string(i));
+    }
+
+    for (int i = 0; i < 100; ++i) {
+        std::cout << deque.popBack() << std::endl;
+    }
+
+    return 0;
+}
